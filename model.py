cd20247dbcb110d6b36b06047eb1380e58536c96
Jeevan-HM <jeevanhm308@gmail.com>
2025-11-24 00:55:22 -0700

    Create leg structure

--- model.py
+++ model.py
@@ -5,7 +5,6 @@ model = mujoco.MjModel.from_xml_path("jansen_linkage.xml")
 data = mujoco.MjData(model)
 
 # Set constant velocity for the crank (30 units/sec)
-data.ctrl[0] = 30
 
 with mujoco.viewer.launch_passive(model, data) as viewer:
     while viewer.is_running():
